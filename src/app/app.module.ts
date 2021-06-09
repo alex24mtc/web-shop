4a07367d1c114efe8924774f4eb08d5fa4608164
alex24mtc <alex25mtc@gmail.com>
2021-06-09 15:20:59 +0200

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//components created
+import { CheckoutPageComponent } from './components/checkout-page/checkout-page.component';
+import { ShopPageComponent } from './components/shop-page/shop-page.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckoutPageComponent,
+    ShopPageComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
